721d83466bb8845401775e7dd4f3fd26dcd91f25
palsmihe <palsmihe@gmail.com>
2022-11-18 17:08:02 +0800

    re-orgnize structure to make it more resonable

--- conftest.py
+++ conftest.py
@@ -1,18 +1,14 @@
 import pytest
-import bios_update
-def pytest_addoption(parser):
-    parser.addoption('--item',action='store',default='all',help='input the function name you need to test, or input all for all items')
 
-def pytest_generate_tests(metafunc):
-    # print('all=',metafunc.fixturenames)
+# def pytest_addoption(parser):
+#     parser.addoption('--item',action='store',default='all',help='input the function name you need to test, or input all for all items')
+#
+# def pytest_generate_tests(metafunc):
+#     # print('all=',metafunc.fixturenames)
+#
+#     option_value=metafunc.config.option.item
+#     # print('value=',option_value)
+#     if 'item' in metafunc.fixturenames and option_value is not None:
+#         metafunc.parametrize('item',[option_value])
+#
 
-    option_value=metafunc.config.option.item
-    # print('value=',option_value)
-    if 'item' in metafunc.fixturenames and option_value is not None:
-        metafunc.parametrize('item',[option_value])
-
-# @pytest.fixture(autouse=True)
-# def bios_load_default():
-#     bios = bios_update.Action()
-#     bios.set_item(None, None, 'default')
-#     bios.action()
